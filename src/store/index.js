14554a5e95090fff24c73b5ad74d953b02b2d338
Vaios Anastasiadis <AnastasiadisV@kotsovolos.gr>
2021-09-07 15:19:21 +0300

    Add redux dev tools extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import { createEpicMiddleware } from 'redux-observable';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from 'models/reducers/rootReducer';
 import rootEpics from 'models/epics/rootEpics';
 
 const epicMiddleWare = createEpicMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(epicMiddleWare));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(epicMiddleWare)));
 epicMiddleWare.run(rootEpics);
 export default store;
