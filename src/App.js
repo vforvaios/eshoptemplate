fb133dcf4775f59ec663031a10107a24f8900c65
Vaios Anastasiadis <AnastasiadisV@kotsovolos.gr>
2021-09-07 12:59:11 +0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from 'components/home/Home';
+import About from 'components/about/About';
+
+const App = () =>(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home}></Route>
+      <Route path="/about" component={About}></Route>
+    </Switch>
+  </Router>
   );
-}
+
 
 export default App;
