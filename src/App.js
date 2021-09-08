d696e8a49bbd8e590b8a27b0468774a33f0099ae
Vaios Anastasiadis <AnastasiadisV@kotsovolos.gr>
2021-09-08 14:00:05 +0300

    Add common header component to app

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from 'components/home/Home';
 import About from 'components/about/About';
+import Header from 'components/header/Header';
 import 'resources/styles/general.scss';
 
 const App = () => (
   <Router>
+    <Header />
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
