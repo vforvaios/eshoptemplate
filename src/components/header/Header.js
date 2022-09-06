8a77e0443ef6826fdd317d094d877b4644e76274
Vaios Anastasiadis <vaios.anastasiadis@gfk.com>
2022-09-06 16:05:28 +0300

    Add get cart in every route change

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,33 +6,45 @@ import MiniCart from 'components/cart/MiniCart';
 import MainMenu from 'components/main-menu/MainMenu';
 import Search from 'components/searchHeader/Search';
 import { withToggle } from 'library';
-import React from 'react';
+import { getCart } from 'models/actions/cartActions';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import { compose } from 'redux';
 
 import styles from './styles';
 
-const Header = ({ classes, toggleValue, setToggleValue }) => (
-  <Grid container className={classes?.headerContainer}>
-    <Grid item>
-      <MenuIcon
-        onClick={setToggleValue('left', true)}
-        className={classes?.menuIcon}
-      />
-      <Drawer
-        anchor="left"
-        open={toggleValue?.left}
-        onClose={setToggleValue('left', false)}>
-        <MainMenu setToggleValue={setToggleValue} />
-      </Drawer>
+const Header = ({ classes, toggleValue, setToggleValue }) => {
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCart());
+  }, [location]);
+
+  return (
+    <Grid container className={classes?.headerContainer}>
+      <Grid item>
+        <MenuIcon
+          onClick={setToggleValue('left', true)}
+          className={classes?.menuIcon}
+        />
+        <Drawer
+          anchor="left"
+          open={toggleValue?.left}
+          onClose={setToggleValue('left', false)}>
+          <MainMenu setToggleValue={setToggleValue} />
+        </Drawer>
+      </Grid>
+      <Grid item>LOGO</Grid>
+      <Grid item>
+        <div className={classes.headerActions}>
+          <Search />
+          <MiniCart />
+        </div>
+      </Grid>
     </Grid>
-    <Grid item>LOGO</Grid>
-    <Grid item>
-      <div className={classes.headerActions}>
-        <Search />
-        <MiniCart />
-      </div>
-    </Grid>
-  </Grid>
-);
+  );
+};
 
 export default compose(withToggle, withStyles(styles))(Header);
