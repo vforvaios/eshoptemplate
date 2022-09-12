f1433b49e6d21be488e5a18984cf94587819a812
Vaios Anastasiadis <vaios.anastasiadis@gfk.com>
2022-09-12 15:32:27 +0300

    Fix some warnings

--- src/components/checkout/CheckoutStep2.js
+++ src/components/checkout/CheckoutStep2.js
@@ -1,7 +1,23 @@
+import CheckoutStepper from 'components/checkout/CheckoutStepper';
 import React from 'react';
 
 const CheckoutStep2 = () => {
-  return <div>Step 2</div>;
+  return (
+    <div className="content checkout step3">
+      <div className="row">
+        <div className="wrapper">
+          <div className="page-title text-center">
+            <h1>Billing/Shipping</h1>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="wrapper">
+          <CheckoutStepper step="2" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CheckoutStep2;
--- src/components/checkout/CheckoutStepper.js
+++ src/components/checkout/CheckoutStepper.js
@@ -3,10 +3,10 @@ import React from 'react';
 const CheckoutStepper = ({ step }) => (
   <div className="checkout-stepper-container">
     <ul>
-      <li className={(step === '1' || !step) && 'active'}>1</li>
-      <li className={step === '2' && 'active'}>2</li>
-      <li className={step === '3' && 'active'}>3</li>
-      <li className={step === '4' && 'active'}>4</li>
+      <li className={step === '1' || !step ? 'active' : null}>1</li>
+      <li className={step === '2' ? 'active' : null}>2</li>
+      <li className={step === '3' ? 'active' : null}>3</li>
+      <li className={step === '4' ? 'active' : null}>4</li>
     </ul>
   </div>
 );
