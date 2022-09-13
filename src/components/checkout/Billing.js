99b377d3956b5033dd0100116906d021fe7ff5c6
Vaios Anastasiadis <vaios.anastasiadis@gfk.com>
2022-09-13 15:47:27 +0300

    Add a lot of stuff for checkout edit page

--- src/components/checkout/Billing.js
+++ src/components/checkout/Billing.js
@@ -1,57 +1,87 @@
 import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import React from 'react';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import React, { useState } from 'react';
+
+import BillingShippingInputs from './BillingShippingInputs';
 
 const Billing = () => {
+  const [receipt, setReceipt] = useState('receipt');
+  const [sameAsBilling, setSameAsBilling] = useState(true);
+  const [billingInfo, setBillingInfo] = useState({
+    name: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    mobile: '',
+    address: '',
+    postCode: '',
+    city: '',
+    region: '',
+  });
+
+  const [shippingInfo, setShippingInfo] = useState({
+    name: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    mobile: '',
+    address: '',
+    postCode: '',
+    city: '',
+    region: '',
+  });
+
+  const setBillingInformation = (key, value) => {
+    setBillingInfo({ ...billingInfo, [key]: value });
+  };
+
+  const setShippingInformation = (key, value) => {
+    setShippingInfo({ ...shippingInfo, [key]: value });
+  };
+
   return (
     <div className="billing-shipping billing">
-      <div className="receipt-container">checkboxes</div>
-      <div className="billing-inputs">
-        <FormControl fullWidth>
-          <InputLabel htmlFor="name">NAME</InputLabel>
-          <Input
-            fullWidth
-            id="name"
-            type="text"
-            value={''}
-            error={false}
-            onChange={() => {}}
-          />
+      <div className="receipt-container">
+        <FormControl>
+          <RadioGroup
+            row
+            aria-labelledby="receipt-or-invoice"
+            value={receipt}
+            onChange={(e) => setReceipt(e.target.value)}
+            name="receipt-buttons-group">
+            <FormControlLabel
+              value="receipt"
+              control={<Radio />}
+              label="Receipt"
+            />
+            <FormControlLabel
+              value="invoice"
+              control={<Radio />}
+              label="Invoice"
+            />
+          </RadioGroup>
         </FormControl>
-        <FormControl fullWidth>
-          <InputLabel htmlFor="lastName">LAST NAME</InputLabel>
-          <Input
-            fullWidth
-            id="lastName"
-            type="text"
-            value={''}
-            error={false}
-            onChange={() => {}}
+      </div>
+      <div className="flex-billing-shipping">
+        <div className="billing-inputs">
+          <BillingShippingInputs
+            billing
+            inputs={billingInfo}
+            sameAsBilling={false}
+            setInfo={setBillingInformation}
           />
-        </FormControl>
-        <FormControl fullWidth>
-          <InputLabel htmlFor="email">EMAIL</InputLabel>
-          <Input
-            fullWidth
-            id="email"
-            type="email"
-            value={''}
-            error={false}
-            onChange={() => {}}
+        </div>
+        <div className="billing-inputs">
+          <BillingShippingInputs
+            billing={false}
+            sameAsBilling={sameAsBilling}
+            inputs={shippingInfo}
+            setInfo={setShippingInformation}
+            setSameAsBilling={setSameAsBilling}
           />
-        </FormControl>
-        <FormControl fullWidth>
-          <InputLabel htmlFor="phone">PHONE</InputLabel>
-          <Input
-            fullWidth
-            id="phone"
-            type="text"
-            value={''}
-            error={false}
-            onChange={() => {}}
-          />
-        </FormControl>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/checkout/BillingShippingInputs.js
@@ -0,0 +1,147 @@
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import React from 'react';
+
+const BillingShippingInputs = ({
+  billing,
+  inputs,
+  setInfo,
+  sameAsBilling,
+  setSameAsBilling,
+}) => {
+  const {
+    name,
+    lastName,
+    email,
+    phone,
+    mobile,
+    address,
+    postCode,
+    city,
+    region,
+  } = inputs;
+
+  return (
+    <div>
+      {!billing && (
+        <>
+          <FormControlLabel
+            label="Same as billing"
+            control={
+              <Checkbox
+                checked={sameAsBilling}
+                onChange={() => setSameAsBilling(!sameAsBilling)}
+              />
+            }
+          />
+        </>
+      )}
+      <div className={`${!billing && sameAsBilling ? 'same-as-billing' : ''}`}>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="name">NAME</InputLabel>
+          <Input
+            fullWidth
+            id="name"
+            type="text"
+            value={name}
+            error={false}
+            onChange={(e) => setInfo('name', e.target.value)}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="lastName">LAST NAME</InputLabel>
+          <Input
+            fullWidth
+            id="lastName"
+            type="text"
+            value={lastName}
+            error={false}
+            onChange={(e) => setInfo('lastName', e.target.value)}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="email">EMAIL</InputLabel>
+          <Input
+            fullWidth
+            id="email"
+            type="email"
+            value={email}
+            error={false}
+            onChange={(e) => setInfo('email', e.target.value)}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="phone">PHONE</InputLabel>
+          <Input
+            fullWidth
+            id="phone"
+            type="text"
+            value={phone}
+            error={false}
+            onChange={(e) => setInfo('phone', e.target.value)}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="mobile">MOBILE</InputLabel>
+          <Input
+            fullWidth
+            id="mobile"
+            type="text"
+            value={mobile}
+            error={false}
+            onChange={(e) => setInfo('mobile', e.target.value)}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="address">ADDRESS</InputLabel>
+          <Input
+            fullWidth
+            id="address"
+            type="text"
+            value={address}
+            error={false}
+            onChange={(e) => setInfo('address', e.target.value)}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="postCode">POST CODE</InputLabel>
+          <Input
+            fullWidth
+            id="postCode"
+            type="text"
+            value={postCode}
+            error={false}
+            onChange={(e) => setInfo('postCode', e.target.value)}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="city">CITY</InputLabel>
+          <Input
+            fullWidth
+            id="city"
+            type="text"
+            value={city}
+            error={false}
+            onChange={(e) => setInfo('city', e.target.value)}
+          />
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="region">REGION</InputLabel>
+          <Input
+            fullWidth
+            id="region"
+            type="text"
+            value={region}
+            error={false}
+            onChange={(e) => setInfo('region', e.target.value)}
+          />
+        </FormControl>
+      </div>
+    </div>
+  );
+};
+
+export default BillingShippingInputs;
