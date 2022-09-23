0cad353e2300a85690284f6a4b4190b3fdff4944
Vaios Anastasiadis <vaios.anastasiadis@gfk.com>
2022-09-23 16:59:09 +0300

    Add more staff in product page

--- src/components/product-page/ProductPage.js
+++ src/components/product-page/ProductPage.js
@@ -1,5 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import getPercentage from 'library/getPercentage';
+import { addToCart } from 'models/actions/cartActions';
 import { getProductDetails } from 'models/actions/catalogActions';
 import { singleProduct } from 'models/selectors/catalogSelectors';
 import React, { useEffect } from 'react';
@@ -12,6 +15,14 @@ const ProductPage = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   const product = useSelector(singleProduct);
+  const {
+    productTitle,
+    productDescription,
+    initialPrice,
+    price,
+    imgHref,
+    code,
+  } = product;
 
   useEffect(() => {
     dispatch(getProductDetails(id));
@@ -24,12 +35,42 @@ const ProductPage = () => {
           <Grid container>
             <Grid item xs={6} className="mainProductPhotosContainer">
               <img
-                src={product.imgHref}
+                src={imgHref}
                 alt="Chris standing up holding his daughter Elva"
               />
             </Grid>
             <Grid item xs={6}>
-              {product?.productTitle}
+              <div className="product-title">
+                <h1>{productTitle}</h1>
+                <span className="discount">
+                  {getPercentage(initialPrice, price)}%
+                </span>
+              </div>
+              <div className="price-container">
+                <Typography
+                  variant="body2"
+                  component="p"
+                  className="product-code">
+                  <span>{code}</span>
+                </Typography>
+                <div>
+                  <span>{initialPrice}€</span>
+                  {price}€
+                </div>
+              </div>
+              <Typography component="p" variant="body1">
+                {productDescription}
+              </Typography>
+              <div className="product-page-actions">
+                <button
+                  className="add-to-cart"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    dispatch(addToCart(product));
+                  }}>
+                  ΠΡΟΣΘΗΚΗ ΣΤΟ ΚΑΛΑΘΙ
+                </button>
+              </div>
             </Grid>
           </Grid>
         </div>
