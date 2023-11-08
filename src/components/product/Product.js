1afe2e4284fdd8e4b3d0739a6bdc29eea2db119c
Vaios Anastasiadis <vaios.anastasiadis@newcrosshealthcare.com>
2023-11-08 15:59:10 +0200

    Fixes

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -4,7 +4,6 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import { withStyles } from '@mui/styles';
 import formatMoney from 'library/formatMoney';
 import getPercentage from 'library/getPercentage';
