d756c11388646c62211493eb95fb5600149367e3
Vaios Anastasiadis <vaios_a@outlook.com>
2024-02-29 22:38:27 +0200

    Fetch logo inside menu

--- src/components/main-menu/MainMenu.js
+++ src/components/main-menu/MainMenu.js
@@ -30,7 +30,7 @@ const MainMenu = ({ setToggleValue }) => {
     <div className="main-menu">
       <div className="main-menu-header">
         <img
-          src={logoImage?.preview}
+          src={`${process.env.REACT_APP_API}/${logoImage?.data.name}`}
           alt={logoImage?.data?.name}
           className="logo-image"
         />
