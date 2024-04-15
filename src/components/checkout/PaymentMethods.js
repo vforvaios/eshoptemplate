534aab9cd1a9cffb320396d633757b2f938acef9
Vaios Anastasiadis <vaios_a@outlook.com>
2024-04-15 16:33:37 +0300

    Add info icon for bank deposit

--- src/components/checkout/PaymentMethods.js
+++ src/components/checkout/PaymentMethods.js
@@ -1,21 +1,55 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import FormControl from '@mui/material/FormControl';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import Popover from '@mui/material/Popover';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import formatMoney from 'library/formatMoney';
 import { checkPaymentMethod } from 'models/actions/checkoutActions';
 import { paymentMethods } from 'models/selectors/checkoutSelectors';
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 const PaymentMethods = () => {
   const dispatch = useDispatch();
+  const [anchorEl, setAnchorEl] = useState(null);
   const currentPaymentMethods = useSelector(paymentMethods);
+
+  const paymentMethodForDepositDescription = currentPaymentMethods?.find(
+    (p) => p?.description,
+  );
+
+  const handlePaymentInfoClicked = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   return (
     <div className="payment-methods">
-      <h3>PAYMENT METHODS</h3>
+      <h3>
+        PAYMENT METHODS{' '}
+        <span className="info-icon" onClick={handlePaymentInfoClicked}>
+          &#128712;
+        </span>
+        <Popover
+          overlayClassName="info-popover"
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}>
+          {paymentMethodForDepositDescription?.description}
+        </Popover>
+      </h3>
       <FormControl>
         <RadioGroup
           aria-labelledby="payment-methods"
@@ -27,10 +61,11 @@ const PaymentMethods = () => {
               key={paymentmethod?.id}
               value={paymentmethod?.name}
               control={<Radio />}
-              label={`${paymentmethod?.name} ${paymentmethod?.cost > 0
+              label={`${paymentmethod?.name} ${
+                paymentmethod?.cost > 0
                   ? `(${formatMoney.format(paymentmethod.cost)})`
                   : ''
-                }`}
+              }`}
             />
           ))}
         </RadioGroup>
