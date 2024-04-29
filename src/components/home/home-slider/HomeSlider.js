eef4ea916e70c8dda661025a2275f2fa145bcd75
Vaios Anastasiadis <vaios_a@outlook.com>
2024-04-29 16:42:55 +0300

    Add video tag in home slider

--- src/components/home/home-slider/HomeSlider.js
+++ src/components/home/home-slider/HomeSlider.js
@@ -5,36 +5,39 @@ const HomeSlider = ({ banners }) => {
   return (
     <div className="accordion">
       <ul>
-        {banners?.map((banner, index) => (
-          <li
-            key={banner.id}
-            tabIndex={index}
-            style={{
-              backgroundImage: `url('${process.env.REACT_APP_IMAGES_URL}/${banner.imgHref}')`,
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: 'center center',
-              backgroundSize: 'cover',
-            }}>
-            <div>
-              <Link to={banner.link}>
-                <h2>{banner.title}</h2>
-              </Link>
-            </div>
-          </li>
-        ))}
-        <li className="videoLi">
-          <div>
-            <video autoPlay loop muted>
-              <source
-                type="video/mp4"
-                src="http://api.tierrapurses.gr/images/samplevideo.mp4"
-              />
-            </video>
-            <Link to="/">
-              <h2>Hi</h2>
-            </Link>
-          </div>
-        </li>
+        {banners?.map((banner, index) =>
+          !banner?.isVideo ? (
+            <li
+              key={banner.id}
+              tabIndex={index}
+              style={{
+                backgroundImage: `url('${process.env.REACT_APP_IMAGES_URL}/${banner.imgHref}')`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center center',
+                backgroundSize: 'cover',
+              }}>
+              <div>
+                <Link to={banner.link}>
+                  <h2>{banner.title}</h2>
+                </Link>
+              </div>
+            </li>
+          ) : (
+            <li className="videoLi">
+              <div>
+                <video autoPlay loop muted>
+                  <source
+                    type="video/mp4"
+                    src={`${process.env.REACT_APP_IMAGES_URL}/${banner.imgHref}`}
+                  />
+                </video>
+                <Link to={banner.link}>
+                  <h2>{banner.title}</h2>
+                </Link>
+              </div>
+            </li>
+          ),
+        )}
       </ul>
     </div>
   );
